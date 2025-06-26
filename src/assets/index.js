a6c037c61d59708047fbf93e34503e0b052cdbb3
Simon-Olivier Bolduc <simon-olivierbolduc@solutioncpa.com>
2025-06-26 10:20:02 -0400

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,99 @@
+import logo from "./logo.svg";
+import tiger from "./tiger.svg";
+import glasses from "./glasses.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import C from "./C++.png";
+import VBA from "./VBA.svg";
+import Python from "./python.svg";
+import brain from "./brain.svg";
+import WebDev from "./WebDev.svg"; 
+import vs from "./vs.svg";
+import CPA from "./company/CPALOGO.svg";
+import Fleur from "./company/fleur.png";
+import Basket from "./company/noiretor.jpg";
+import Ville from "./company/sag.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import BI from "./tech/BI.svg";
+import powerpages from "./tech/powerpages.svg";
+import powerapps from "./tech/powerapps.svg";
+import powerautomate from "./tech/powerautomate.svg";
+import dataverse from "./tech/dataverse.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import PowerPlatform from "./PowerPlatform.png";
+
+
+export {
+  logo,
+  Ville,
+  BI,
+  powerpages,
+  powerapps,
+  powerautomate,
+  dataverse,
+  C,
+  vs,
+  Fleur,
+  CPA,
+  Basket,
+  Python,
+  brain,
+  WebDev,
+  VBA,
+  tiger,
+  glasses,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  PowerPlatform
+};
---
+++ src/constants/index.js
@@ -0,0 +1,272 @@
+import {
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  docker,
+
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  PowerPlatform,
+  brain,
+  vs,
+  WebDev,
+  CPA,
+  C,
+  Python,
+  VBA,
+  Fleur,
+  Basket,
+  Ville,
+  powerpages,
+  powerapps,
+  powerautomate,
+  dataverse,
+  BI,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "À propos",
+  },
+  {
+    id: "work",
+    title: "Travail",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Exploitation Power Platform",
+    icon: PowerPlatform,
+  },
+  {
+    title: "Développement web et application",
+    icon: WebDev,
+  },
+  {
+    title: "Autonomne, curieux et créatif",
+    icon: brain,
+  },
+  {
+    title: "À l'aise avec plusieurs logiciels",
+    icon: vs,
+  },
+];
+
+const technologies = [
+  {
+    name: "C++",
+    icon: C,
+  },
+  {
+    name: "Python",
+    icon: Python,
+  },
+  {
+    name: "VBA",
+    icon: VBA,
+  },
+  
+  {
+    name: "Power Automate",
+    icon: powerautomate,
+  },
+  {
+    name: "Dataverse",
+    icon: dataverse,
+  },
+  {
+    name: "BI",
+    icon: BI,
+  },
+  {
+    name: "Power Apps",
+    icon: powerapps,
+  },
+  {
+    name: "Power Pages",
+    icon: powerpages,
+  },
+  
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Stagiaire en génie informatique",
+    company_name: "Centre de Production Automatisé",
+    icon: CPA,
+    iconBg: "#E6DEDD",
+    date: "Mai 2024 - Actuel",
+    points: [
+      "Création d’applications et sites Web.",
+      "Traitement de données et travaux sur les bases de données.",
+      "Migration de logiciels tels que ControlLogix.",
+      "Exploitation de Microsoft Power Platform.",
+    ],
+  },
+  {
+    title: "Employé général",
+    company_name: "CIUSS du Saguenay-Lac-Saint-Jean",
+    icon: Fleur,
+    iconBg: "#1b6a79",
+    date: "Janvier 2021 - Octobre 2024",
+    points: [
+      "Préposé(e) au service alimentaire.",
+      "Surveillance en CHSLD et Centre hospitalier.",
+      "Préposé(e) à l'hygiène et salubrité.",
+      "Préposé(e) à l’accueil .",
+      "Préposé(e) à la buanderie.",
+      "Aide de service à l’urgence.",
+    ],
+  },
+  {
+    title: "Coach Sportif",
+    company_name: "Club de Basketball de Chicoutimi",
+    icon: Basket,
+    iconBg: "#000000",
+    date: "Octobre 2022 - Mai 2024",
+    points: [
+      "Encadrement des jeunes d’une équipe de basketball.",
+      "Gestion des tournois à l’externe et à l’interne.",
+    ],
+  },
+  {
+    title: "Animateur de camp de jour",
+    company_name: "Ville de Saguenay",
+    icon: Ville,
+    iconBg: "#fafafa",
+    date: "Juillet 2020 - Août 202",
+    points: [
+      "Élaboration d’un programme d’activités adaptées à l’âge et aux besoins des enfants en collaboration avec l’équipe danimation.",
+      "Prise en charge d'un groupe de 10 à 20 enfants âgés de 7 et 8 ans.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
